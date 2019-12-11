24b8c43d6965483bbbd49efdfdbaf3e4b1cab348
Eduardo Rosas <lalorosas@outlook.com>
2019-12-11 16:46:56 -0600

    Define and use state on a class Component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // while props are set from outside
+  // state is managed from inside the component
+  state = {
+    persons: [
+      { name: 'Eduardo', age: 26 },
+      { name: 'Yesenia', age: 26 },
+      { name: 'Marty', age: 60 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!!!</p>
-        <Person name="Eduardo" age="26" />
-        <Person name="Yesenia" age="26" >Some extra information about me.</Person>
-        <Person name="Marty" age="60" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Some extra information about me.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
