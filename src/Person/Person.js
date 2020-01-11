bbdcb414fd8537da99dc86eae9fc4d0b5644c4bc
Eduardo Rosas <lalorosas@outlook.com>
2020-01-11 13:31:59 -0600

    CSS Modules & Media Queries
    Media Queries are used as normal CSS when using CSS Modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-
-// This is a component because the div function
-// from styled already returns a component
-// no need to make StyleDiv a class-based or functional
-// component, it is already assigned to one.
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import cssClasses from './Person.css'
 
 const person = (props) => {
     return (
-        // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={cssClasses.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
-        //</div>
+        </div>
     ); 
 };
 
