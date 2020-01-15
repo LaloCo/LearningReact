2ba333ffb50f60ed10bfe8630286b4dd6143b5af
Eduardo Rosas <lalorosas@outlook.com>
2020-01-15 12:44:36 -0600

    Change to class components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 import Person from './Person/Person';
 
-const Persons = props => props.persons.map((person, index) => {
-    return <ErrorBoundary key={person.id}>
-                <Person name={person.name}
-                    age={person.age}
-                    click={() => props.clicked(index)}
-                    changed={(event) => props.changed(event, person.id)}/>
-            </ErrorBoundary>
-});
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}>
+                        <Person name={person.name}
+                            age={person.age}
+                            click={() => this.props.clicked(index)}
+                            changed={(event) => this.props.changed(event, person.id)}/>
+                    </ErrorBoundary>
+        });
+    }
+}
 
 export default Persons;
